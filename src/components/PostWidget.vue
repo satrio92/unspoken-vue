2ce07093b4e090e3c8d01661bba9b88062717831
satrio92 <kukuhsatrio920@gmail.com>
2023-01-31 08:17:01 +0700

    add post widget

---
+++ src/components/PostWidget.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="post-widget" class="flex flex-col items-center gap-5 bg-black-gradient rounded-[20px] py-7 w-full text-white">
+        <h4 class="text-[28px] font-bold">Recent Post</h4>
+        <div class="flex flex-col">
+            <div class="flex items-center gap-3 py-4 border-t border-t-white" v-for="item in 3" :key="item.id">
+                <img src="#" alt="#" class="w-[40px] h-[40px] rounded-full bg-pink-300">
+                <div class="flex flex-col w-[220px] gap-1">
+                    <p class="text-xs font-medium">Money makes your life easier. If you're lucky to have it, you're lucky.</p>
+                    <p class="text-[10px]">{{ postData[0].postDate }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostWidget",
+    components: {},
+    props: ['postData']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
-	<div id="home" class="w-full h-[1000px] px-36 bg-black-gradient font-poppins">
+	<div id="home" class="w-full h-[2000px] px-12 bg-black-gradient font-poppins">
 		<header>
 			<Navbar />
 			<FeaturedPost :postData="postData"/>
 		</header>
+		<div class="w-full flex gap-[52px] relative justify-center">
+			<!-- POSTS -->
+			<div class="w-[776px] h-[1000px] bg-pink-300"></div>
+			<!-- Posts Widget -->
+			<div class="w-[370px] h-[605px] sticky top-6 flex flex-col justify-between">
+				<PostWidget :postData="postData"/>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -11,13 +19,15 @@
 
 import FeaturedPost from '../components/FeaturedPost.vue';
 import Navbar from '../components/Navbar.vue'
+import PostWidget from '../components/PostWidget.vue';
 import postData from '../posts'
 
 export default {
 	name: 'Home',
 	components: {
-    Navbar,
-    FeaturedPost
+		Navbar,
+		FeaturedPost,
+		PostWidget
 	},
 	data() {
 		return {
