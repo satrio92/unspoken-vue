e6335547f3dcd0a4a4de82cf455f79ae9f72da3e
satrio92 <kukuhsatrio920@gmail.com>
2023-02-06 06:38:05 +0700

    add size screen guidline

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@
 </script>
 
 <template>
+  <div class="fixed z-50">
+    <div class="absolute top-0 left-0 p-2 text-white font-bold bg-blue-600 text-sm">
+      <span class="block sm:hidden">XS</span>
+      <span class="hidden sm:block md:hidden">SM</span>
+      <span class="hidden md:block lg:hidden">MD</span>
+      <span class="hidden lg:block xl:hidden">LG</span>
+      <span class="hidden xl:block 2xl:hidden">XL</span>
+      <span class="hidden 2xl:block">2XL</span>
+    </div>
+  </div>
   <router-view />
 </template>
