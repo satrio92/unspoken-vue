dcff949e828a069ff5892b1bee796e3ef1643e9b
satrio92 <kukuhsatrio920@gmail.com>
2023-02-06 15:55:33 +0700

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,6 +12,7 @@
 				</router-link>
 				<router-link to="/about" class="py-3 px-7 xl:px-12 flex text-sm xl:text-base items-center relative" active-class="font-semibold text-[#050915] bg-dropsunrise" exact>
 					<span>About</span>
+					<div v-if="$route.path === '/about'" class="border-[3px] border-white h-[14px] w-[14px] rounded-full bg-black absolute -right-[7px]"></div>
 					<div v-if="$route.path === '/about'" class="border-[3px] border-white h-[14px] w-[14px] rounded-full bg-black absolute -left-[7px]"></div>
 				</router-link>
 				<router-link to="/contact" class="py-3 px-7 xl:px-12 flex text-sm xl:text-base items-center relative" active-class="font-semibold text-[#050915] bg-dropsunrise" exact>
